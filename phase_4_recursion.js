36f8424681790dc9e4135919f48128194143ab00
Rohan Mudumba <roh20rocks@gmail.com>
2023-05-16 16:16:23 -0700

    halfway phase4

--- phase_4_recursion.js
+++ phase_4_recursion.js
@@ -1,1 +1,35 @@
 // Your code here
+function range(start, end) {
+    if (start === end) {
+        return [start];
+    }
+    return [start].concat(range(start + 1, end));
+}
+
+function sumRec(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    return arr[0] + sumRec(arr.slice(1, arr.length));
+}
+
+function exp1(b, n) {
+    if (n === 0) {
+        return 1;
+    }
+    return b * exp1(b, n-1);
+}
+
+function exp2(b, n) {
+    if (n === 0) {
+        return 1;
+    }
+    if (n === 1) {
+        return b;
+    }
+    if (n%2 === 0) {
+        return exp2(b, n/2) **2;
+    }else {
+        return b * (exp2(b, (n-1)/2) **2);
+    }
+}
