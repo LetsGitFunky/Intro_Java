93d2ec16a1b2d69c2a1ce1a2c78245df31c914e1
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-16 15:25:07 -0700

    Phase 3

--- phase_3_iteration.js
+++ phase_3_iteration.js
@@ -1,1 +1,34 @@
-// Your code here
+Array.prototype.bubbleSort = function() {
+    let result = [];
+    this.forEach(function(el) {
+        result.push(el);
+    })
+    let swapped = true;
+    while (swapped === true) {
+        swapped = false;
+        for (let i = 0; i < result.length -1; i++) {
+            if (result[i+1] < result[i]) {
+                let temp = result[i];
+                result[i] = result[i+1];
+                result[i+1] = temp;
+                swapped = true;
+            }
+        }
+    }
+    return result;
+}
+
+String.prototype.substrings = function() {
+    let result = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let temp = "";
+        for (let j = i; j < this.length; j++) {
+            temp += this[j];
+            result.push(temp);
+        }
+    }
+    return result;
+}
+
+
